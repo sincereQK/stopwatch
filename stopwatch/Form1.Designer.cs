46b46aea6f956348828a848563ac6b9d09d963ca
sincereQK <mattmingyu@gmail.com>
2019-03-02 21:10:42 +0900

    QK stopwatch basic Design complete need to set detail / first fix

--- stopwatch/Form1.Designer.cs
+++ stopwatch/Form1.Designer.cs
@@ -29,12 +29,70 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.lblST = new System.Windows.Forms.Label();
+            this.buttStart = new System.Windows.Forms.Button();
+            this.buttCancel = new System.Windows.Forms.Button();
+            this.timerST = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblST
+            // 
+            this.lblST.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblST.Font = new System.Drawing.Font("굴림", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblST.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblST.Location = new System.Drawing.Point(32, 84);
+            this.lblST.Name = "lblST";
+            this.lblST.Size = new System.Drawing.Size(481, 82);
+            this.lblST.TabIndex = 0;
+            this.lblST.Text = "00:00:00.0000000";
+            this.lblST.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // buttStart
+            // 
+            this.buttStart.BackColor = System.Drawing.Color.Lavender;
+            this.buttStart.Location = new System.Drawing.Point(542, 84);
+            this.buttStart.Name = "buttStart";
+            this.buttStart.Size = new System.Drawing.Size(109, 38);
+            this.buttStart.TabIndex = 1;
+            this.buttStart.Text = "Start";
+            this.buttStart.UseVisualStyleBackColor = false;
+            this.buttStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttCancel
+            // 
+            this.buttCancel.BackColor = System.Drawing.Color.Red;
+            this.buttCancel.Location = new System.Drawing.Point(542, 128);
+            this.buttCancel.Name = "buttCancel";
+            this.buttCancel.Size = new System.Drawing.Size(109, 38);
+            this.buttCancel.TabIndex = 2;
+            this.buttCancel.Text = "Cancel";
+            this.buttCancel.UseVisualStyleBackColor = false;
+            // 
+            // timerST
+            // 
+            this.timerST.Interval = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1006, 495);
+            this.Controls.Add(this.buttCancel);
+            this.Controls.Add(this.buttStart);
+            this.Controls.Add(this.lblST);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblST;
+        private System.Windows.Forms.Button buttStart;
+        private System.Windows.Forms.Button buttCancel;
+        private System.Windows.Forms.Timer timerST;
     }
 }
 
--- stopwatch/Form1.cs
+++ stopwatch/Form1.cs
@@ -12,9 +12,40 @@ namespace stopwatch
 {
     public partial class Form1 : Form
     {
+        private DateTime startTime, startTime2;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            if (buttStart.Text == "Start")
+            {
+                buttStart.Text = "Pause";
+                startTime = DateTime.Now;
+            }
+            else if (buttStart.Text == "Pause")
+            {
+                buttStart.Text = "Start";
+                startTime2 = DateTime.Parse(lblST.Text);
+            }
+
+            timerST.Enabled = !timerST.Enabled;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            startTime2 = default(DateTime);
+            lblST.Text = ("00:00:00.0000");
+            timerST.Stop();
+            buttStart.Text = "Start";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TimeSpan span = new TimeSpan(DateTime.Now.Ticks - startTime.Ticks + startTime2.Ticks);
+            lblST.Text = span.ToString("hh\\.mm\\.ss\\.ffff");
         }
     }
 }
